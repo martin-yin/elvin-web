4bb25fb109c60cbc756ebaf375a7ae6eeeeece45
Martin <martin-yin@foxmail.com>
2021-04-27 13:46:18 +0800

    commit

--- src/view/HomePage/home.page.tsx
+++ src/view/HomePage/home.page.tsx
@@ -1,5 +1,5 @@
 import { AppstoreOutlined, EditOutlined } from '@ant-design/icons'
-import { Space, Tooltip } from 'antd'
+import { Card, Col, Row, Space, Tooltip } from 'antd'
 import React, { FC } from 'react'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
@@ -20,59 +20,65 @@ const HomePage: FC = () => {
     <>
       <div>
         <div className="project-list">
-          {projectList.length > 0 ? (
-            projectList.map((item: any, index) => {
-              return (
-                <div className="project-item" key={index}>
-                  <div className="item-title">
-                    <div className="project-title-name flex-grow-1">{item.project_name}</div>
-                    <div className="project-title-operation flex-grow-1">
-                      <Space size={20}>
-                        <Tooltip placement="topLeft" title="修改配置">
-                          <EditOutlined style={{ color: '#a3a5b0' }} />
-                        </Tooltip>
-                        <p onClick={() => projectSurvey(item.monitor_id)}>
-                          <Tooltip placement="topLeft" title="查看概况">
-                            <AppstoreOutlined style={{ color: '#a3a5b0' }} />
-                          </Tooltip>
-                        </p>
-                      </Space>
-                    </div>
-                  </div>
-                  <div className="item-content">
-                    <div className="project-quota">
-                      <div className="quota-item">
-                        <p style={{ color: '#ff6a00' }}>8000</p>
-                        <span className="quota-item-title">今日访问</span>
+          <Row gutter={[16, 16]}>
+            {projectList.length > 0 ? (
+              projectList.map((item: any, index) => {
+                return (
+                  <Col key={index} span={8}>
+                    <Card>
+                      <div className="project-item">
+                        <div className="item-title">
+                          <div className="project-title-name flex-grow-1">{item.project_name}</div>
+                          <div className="project-title-operation flex-grow-1">
+                            <Space size={20}>
+                              <Tooltip placement="topLeft" title="修改配置">
+                                <EditOutlined style={{ color: '#a3a5b0' }} />
+                              </Tooltip>
+                              <p onClick={() => projectSurvey(item.monitor_id)}>
+                                <Tooltip placement="topLeft" title="查看概况">
+                                  <AppstoreOutlined style={{ color: '#a3a5b0' }} />
+                                </Tooltip>
+                              </p>
+                            </Space>
+                          </div>
+                        </div>
+                        <div className="item-content">
+                          <div className="project-quota">
+                            <div className="quota-item">
+                              <p style={{ color: '#ff6a00' }}>8000</p>
+                              <span className="quota-item-title">今日访问</span>
+                            </div>
+                            <div className="quota-item">
+                              <p>0</p>
+                              <span className="quota-item-title">新用户</span>
+                            </div>
+                            <div className="quota-item">
+                              <p>0</p>
+                              <span className="quota-item-title">老用户</span>
+                            </div>
+                          </div>
+                          <p>健康总分</p>
+                          <div className="flex health">
+                            <div className="flex-grow-0">{/* <HomeChart /> */}</div>
+                            <div className="flex-grow-1 health-rate">
+                              <span>
+                                <label></label> JS报错率：28.08%
+                              </span>
+                              <span>接口报错率：0.23%</span>
+                              <span>资源报错率：5.93%</span>
+                              <span></span>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div className="quota-item">
-                        <p>0</p>
-                        <span className="quota-item-title">新用户</span>
-                      </div>
-                      <div className="quota-item">
-                        <p>0</p>
-                        <span className="quota-item-title">老用户</span>
-                      </div>
-                    </div>
-                    <p>健康总分</p>
-                    <div className="flex health">
-                      <div className="flex-grow-0">{/* <HomeChart /> */}</div>
-                      <div className="flex-grow-1 health-rate">
-                        <span>
-                          <label></label> JS报错率：28.08%
-                        </span>
-                        <span>接口报错率：0.23%</span>
-                        <span>资源报错率：5.93%</span>
-                        <span></span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )
-            })
-          ) : (
-            <></>
-          )}
+                    </Card>
+                  </Col>
+                )
+              })
+            ) : (
+              <></>
+            )}
+          </Row>
         </div>
       </div>
     </>
--- src/view/SurveyPage/survey.page.tsx
+++ src/view/SurveyPage/survey.page.tsx
@@ -1,7 +1,49 @@
+import { InfoCircleFilled } from '@ant-design/icons/lib/icons'
+import { Card, Col, Row, Statistic, Tooltip } from 'antd'
 import React, { FC } from 'react'
 
 const SurveyPage: FC = () => {
-  return <div>项目概况</div>
+  return (
+    <div>
+      <p>核心数据</p>
+      <Card className="header-quota">
+        <p className="quota-tips">
+          <Tooltip title="今日数据指标">
+            <InfoCircleFilled style={{ fontSize: '16px', color: '#3399FF' }} />
+          </Tooltip>
+        </p>
+        <div className="item">
+          <Statistic title="访客数" value={99} suffix="" />
+        </div>
+        <div className="item">
+          <Statistic title="浏览量(PV)" value={100} suffix="" />
+        </div>
+        <div className="item">
+          <Statistic title="IP数" value={10} suffix="" />
+        </div>
+        <div className="item">
+          <Statistic title="Js异常" value={0} suffix="" />
+        </div>
+        <div className="item">
+          <Statistic title="资源异常" value={0} suffix="" />
+        </div>
+      </Card>
+      <div style={{ marginTop: '20px' }}>
+        <Row gutter={[16, 16]}>
+          <Col span={8}>
+            <Card>js异常</Card>
+          </Col>
+          <Col span={8}>
+            <Card>Api成功率</Card>
+          </Col>
+          <Col span={8}>
+            <Card>页面性能</Card>
+          </Col>
+        </Row>
+      </div>
+      <p>综合数据</p>
+    </div>
+  )
 }
 
 export default SurveyPage
